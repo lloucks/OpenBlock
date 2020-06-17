1a5ab1ae61f98a8c6de6306ab86a7512426c3edf
James Lacroix <lacroix.james@icloud.com>
2020-06-17 10:30:02 -0700

    initial block/transaction structure

--- src/block/block.go
+++ src/block/block.go
@@ -31,20 +31,3 @@ type BlockHeader struct{
 
 
 
-//Given that we are not using currency, the transaction strucutre is flexible.
-
-//We can make it so that we have some designated address that all posts are sent to
-
-//Please add to this structure when required to make a part work
-type Transaction struct{
-    text string //contents of the post
-    author int //Will be the authors signature, that other nodes should be able to verify
-    //int is just a placeholder for now
-
-
-    //need to send it somewhere
-    //need to sign it so it can be veried
-
-
-
-}
