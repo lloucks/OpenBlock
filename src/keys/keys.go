6b67ae0dc4bb334e12f394c289b938138fd06b2e
Daniel MacRae <danielm9@uvic.ca>
2020-06-24 08:23:59 -0700

    Changed encoding format to one that supports private/public key pairs and added output of private key to local storage.

--- src/keys/keys.go
+++ src/keys/keys.go
@@ -14,17 +14,19 @@ func GenerateKeys(){
 	   fmt.Println("Failed to generate key pair.")
 	}
 
-	// MarshalPKIXPublicKey converts a public key to PKIX, ASN.1 DER form.
-	pubASN1, err := x509.MarshalPKIXPublicKey(&Priv.PublicKey)
-
-	if err != nil {
-	    fmt.Println("Failed to convert public key.")
-	}
-
+	// MarshalPKCS1__Key converts a key to PKCS#1, ASN.1 DER form.
 	pubBytes := pem.EncodeToMemory( &pem.Block{
 	    Type:  "RSA PUBLIC KEY",
-	    Bytes: pubASN1,
+	    Bytes: x509.MarshalPKCS1PublicKey(&Priv.PublicKey),
 	})
 
+	//WriteFile(filename string, data []byte, perm os.FileMode)
 	ioutil.WriteFile("key.pub", pubBytes, 0644)
+
+	privBytes := pem.EncodeToMemory( &pem.Block{
+	    Type:  "RSA PRIVATE KEY",
+	    Bytes: x509.MarshalPKCS1PrivateKey(Priv),
+	})
+
+	ioutil.WriteFile("key.priv", privBytes, 0644)
 }
