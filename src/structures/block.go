e6ca2509fe61ce7b727c77aaaf930feb1689bf9f
James Lacroix <lacroix.james@icloud.com>
2020-07-07 10:26:06 -0700

    cleaned up block.go

--- src/structures/block.go
+++ src/structures/block.go
@@ -9,12 +9,8 @@ import (
 	"time"
 )
 
-//These data types are not representative of the actual product
-
 type Block struct {
 	Index int
-	//Data string
-	//Data is stored in transactions, not blocks.
 	Magic_num  uint32 //so we know it's part of our protocol
 	Block_size uint32 //number of bytes in this block
 	Header     BlockHeader
@@ -24,52 +20,8 @@ type Block struct {
 
 type BlockHeader struct {
 	Prev_block_hash int
-	//Hash int
-	//you cannot do this
 	Merkle_root_hash int
 	Timestamp        time.Time //created by the node making this block
 	Difficulty       uint32    //the difficulty, how many 0s need to be in PoW hash
 	Nonce            uint32    //the number that solves this block
 }
-
-//These functions are already defined in pow.go
-/*
-
-func CreateBlock() Block{
-    var block Block
-
-    //block.Magic_num = 0x8b665966a5e97c796e268734cfe5eaf0
-    block.Magic_num = 4 //why the hash?? this is just used as an identifier.
-    //block.Block_size = 0
-    block.Header.Timestamp = time.Now() //aready defined
-
-    return block
-}
-func getPrefix(length int) string {
-    letterBytes := 0
-
-    b:= make([]byte, length)
-
-    for i := range b{
-        b[i] = letterBytes[0]
-    }
-    return string(b)
-}
-
-
-
-func (b *Block) Mine(){
-    prefix := getPrefix(b.Difficulty)
-
-    for{
-        b.GenerateHash()
-
-        if strings.HasPrefix(b.Header.Hash.String(),prefix){
-            break
-        } else {
-            b.Header.Nonce = b.Header.Nonce + 1
-            b.GenerateHash()
-        }
-    }
-}
-*/
