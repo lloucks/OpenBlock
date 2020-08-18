5c9467b58406f2fb1803e740226e8398fc9882d0
lloucks <liamloucks@gmail.com>
2020-08-18 14:59:37 -0700

    explaining in comments why 0 Difficulty blocks should be rejected and not valid

--- src/pow/pow.go
+++ src/pow/pow.go
@@ -64,7 +64,13 @@ func GenerateHash(header structures.BlockHeader) [32]byte{
 
 
 func Verify_work(header structures.BlockHeader) bool{
-    //should not allow 0 Difficulty because then nodes can send empty blocks and they get veirified as valid
+    /*
+    should not allow 0 Difficulty because then nodes can send
+    empty blocks and they get veirified as valid.
+    When a node does does have a block in the RPC calls, the node
+    returns an empty block that would be verified as valid without 
+    rejecting 0 Difficulty blocks.
+    */
 	if header.Difficulty == 0 {
 		return false
 	}
